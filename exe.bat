dafcc71e6ff65f16388846af645086f539f8e6ca
Oxoi <oxcdbe@gmail.com>
2024-08-21 00:45:42 +0800

    add debug and release build type

--- exe.bat
+++ exe.bat
@@ -6,3 +6,7 @@ cmake -DCMAKE_BUILD_TYPE=Release .. --fresh
 cmake --build . --config Release
 cmake -DCMAKE_BUILD_TYPE=Release .. --fresh
 cmake --build . --config Release
+cmake -DCMAKE_BUILD_TYPE=Debug .. --fresh
+cmake --build . --config Debug
+cmake -DCMAKE_BUILD_TYPE=Debug .. --fresh
+cmake --build . --config Debug
